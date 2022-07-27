6296673fbdb4dfec9fc8db0eab15ae736542aedc
Evandro Vidéo de Souza Júnior <evsjmao@yahoo.com.br>
2022-07-27 13:37:00 +0000

    Continuação do projeto em Python.

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return render_template('index.html')
+posts = [
+            {
+                "title": "O meu Primeiro Post",
+                "body": "Aqui é o texto do Post",
+                "author": "Feulo",
+                "created": datetime(2022,7,25)
+            },
+            {
+                "title": "O meu Segundo Post",
+                "body": "Aqui é o texto do Post",
+                "author": "Danilo",
+                "created": datetime(2022,7,26)
+            },
+        ]
 
-@app.route("/meucontato")
-def meuContato():
-   return render_template('contatos.html')
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
+# @app.route("/")
+# @app.route("/hello")
+# def hello():
+#    return render_template('index.html')
+
+# @app.route("/meucontato")
+# def meuContato():
+#   return render_template('contatos.html',email="meu_email@mail.com",telefone="(55) 995555-6666")
 
